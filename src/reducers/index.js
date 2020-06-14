600a8342c1371c125944cf3837e56929fe9350e7
himanshigulati <himanshigulati07@gmail.com>
2020-06-14 14:33:47 +0530

    Added articles list api integration

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,23 @@
 import login from './login';
 import article from './articles';
+import articleData from './articleData';
+import common from './common';
+import editor from './editor';
+import home from './home';
+import profile from './profile';
+import settings from './settings';
 import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 const rootReducer = combineReducers({
-    login,
     article,
+    articleData,
+    login,
+    common,
+    editor,
+    home,
+    profile,
+    settings,
     router: routerReducer
 });
 
